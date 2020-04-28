5b88d16e7b3a0c3d30a8acef63d67eb39afcbb6d
Erick Christian <erickchristian48@gmail.com>
2020-04-29 00:06:28 +0700

    Fixed conflict

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,28 @@
 |
 */
 
-<<<<<<< HEAD
+Route::get('/admin', function () {
+    return view('admin.index');
+});
+
+Route::get('/guru', function () {
+    return view('guru.index');
+});
+
+Route::group(['prefix'=>'murid'], function (){
+    Route::get('/', function () {
+        return view('murid.index');
+    });
+    Route::get('/data-diri', function () {
+        return view('murid.datadiri');
+    });
+});
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::group(['prefix' => 'admin'], function() {
     Route::get('/login', 'AdminAuth\LoginController@showLoginForm')->name('admin.login');
     Route::post('/login', 'AdminAuth\LoginController@login')->name('admin.login.submit');
@@ -37,29 +58,3 @@ Route::group(['prefix' => 'ortu'], function() {
         return "Ortu udh login";
     })->name('admin.dashboard');
 });
-
-Route::get('/', function () {
-    return view('welcome');
-=======
-Route::get('/admin', function () {
-    return view('admin.index');
->>>>>>> 5243e7be50d99ad50bd2de233e3e83bf122b6555
-});
-
-Route::get('/guru', function () {
-    return view('guru.index');
-});
-
-Route::group(['prefix'=>'murid'], function (){
-    Route::get('/', function () {
-        return view('murid.index');
-    });
-    Route::get('/data-diri', function () {
-        return view('murid.datadiri');
-    });
-});
-
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
