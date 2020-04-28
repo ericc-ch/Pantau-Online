03e29cf3438b5b16a1c39c478ac2209964e2ee64
Erick Christian <erickchristian48@gmail.com>
2020-04-29 02:02:09 +0700

    Done, middleware menyusul

--- app/Http/Controllers/AdminAuth/LoginController.php
+++ app/Http/Controllers/AdminAuth/LoginController.php
@@ -5,36 +5,39 @@ namespace App\Http\Controllers\AdminAuth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::ADMIN_HOME;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        $this->middleware('guest:admin')->except('logout');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth_admin.login');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = [
+            'username' => $request->username,
+            'password' => $request->password,
+        ];
+ 
+        if (Auth::guard('admin')->attempt($credentials, $request->member))
+        {
+            return redirect()->intended(route('admin.dashboard'));
+        }
+
+        return redirect()->back()->withInput($request->only('username', 'remember'));
     }
 }
--- app/Http/Controllers/GuruAuth/LoginController.php
+++ app/Http/Controllers/GuruAuth/LoginController.php
@@ -5,36 +5,38 @@ namespace App\Http\Controllers\GuruAuth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        $this->middleware('guest:guru')->except('logout');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth_guru.login');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = [
+            'username' => $request->username,
+            'password' => $request->password,
+        ];
+ 
+        if (Auth::guard('guru')->attempt($credentials, $request->member))
+        {
+            return redirect()->intended(route('guru.dashboard'));
+        }
+
+        return redirect()->back()->withInput($request->only('username', 'remember'));
     }
 }
