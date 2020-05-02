b184e6736c8428e8cd3457be3224c89cc37507b3
Erick Christian <erickchristian48@gmail.com>
2020-05-02 22:31:25 +0700

    Test lagi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,6 +62,7 @@
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
+                                
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
