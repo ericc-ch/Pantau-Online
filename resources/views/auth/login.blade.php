510503f9db7053f64fb28507fac662492738eab3
Erick Christian <erickchristian48@gmail.com>
2020-05-03 20:08:53 +0700

    View Login --DONE

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,10 +27,8 @@
   </head>
 
   <body>
-
-
                 
-                    <form method="POST" action="{{ route('login') }}">
+                    <form class="form-signin" method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="text-center mb-4">
@@ -56,20 +54,23 @@
                             @enderror
                           </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                          <div class="form-label-group">
+                            <input
+                              type="password"
+                              name="password"
+                              id="password"
+                              name="password"
+                              class="form-control @error('password') is-invalid @enderror"
+                              placeholder="Password"
+                              required
+                            />
+                            <label for="password">Password</label>
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                          </div>
 
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
@@ -84,17 +85,21 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                            <div class="col-md-8 offset-md-2 text-center">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
                             </div>
+                        </div>
+
+                        <div class="form-group row my-4">
+                          <div class="col-md-8 offset-md-2 text-center">
+                            @if (Route::has('password.request'))
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                          @endif
+                          </div>
                         </div>
                     </form>
                 </div>
@@ -104,7 +109,7 @@
 </div>
 
 
-    <form class="form-signin" method="POST" action="{{ route('login') }}">
+    {{-- <form class="form-signin" method="POST" action="{{ route('login') }}">
       <div class="text-center mb-4">
         <h1 class="h3 mb-3 font-weight-normal">Login</h1>
       </div>
@@ -128,17 +133,7 @@
         @enderror
       </div>
 
-      <div class="form-label-group">
-        <input
-          type="password"
-          name="password"
-          id="password"
-          class="form-control"
-          placeholder="Password"
-          required
-        />
-        <label for="password">Password</label>
-      </div>
+
 
       <div class="checkbox mb-3">
         <label>
@@ -157,4 +152,4 @@
       <p class="mt-5 mb-3 text-muted text-center">© 2020</p>
     </form>
   </body>
-</html>
+</html> --}}
