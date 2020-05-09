93a9424f45ced0b57eea8bf7668d6073ab258942
Dhani <mrmdhni01@gmail.com>
2020-05-09 23:41:24 +0700

    rapihin form cari

--- resources/views/murid/pembuktian.blade.php
+++ resources/views/murid/pembuktian.blade.php
@@ -27,21 +27,23 @@
             <div class="card card-default">
                 <div class="card-header">
                     <div class="row">
-                        <div class="col-md-6">
-                            <h3 class="card-title">Jadwal </h3>
+                        <div class="col-md-12">
+                            <h4 class="display-5">Jadwal </h4>
                         </div>
-                        <div class="col-md-6">
-                            <form action="{{route('pembuktian.alihkan')}}" class="form-data" method="post">
+                        <div class="col-md-12 mb-2 mt-2">
+                            <form action="{{route('pembuktian.alihkan')}}" class="form-inline float-right" method="post">
                             @csrf
-                                <div class="row">
-                                    <div class="col-sm-10">
-                                        <input type="date" name="tanggal" id="tanggal" class="form-control float-lg-right"
-                                            required="true">
-                                    </div>
-                                    <div class="col-sm-2">
-                                        <button type="submit" id="simpan" class="btn btn-primary float-lg-right">
-                                            <i class="fa fa-seacrh"></i> cari
-                                        </button>
+                                <div class="col-md-12">
+                                    <div class="row">
+                                        <div class="form-group">
+                                            <input type="date" name="tanggal" id="tanggal" class="form-control"
+                                                    required="true">
+                                        </div>
+                                        <div class="form-group ml-2">
+                                            <button type="submit" id="simpan" class="btn btn-primary">
+                                                <i class="fa fa-seacrh"></i> cari
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             </form>
